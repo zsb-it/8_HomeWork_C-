ab6658ab065eb5a049e32f15f77df3821a8f20ec
Dmitry Kireev <zzsb@ya.ru>
2022-11-10 21:12:52 +0300

    Result Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -10,3 +10,57 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3}, ");
+            else Console.WriteLine($"{matrix[i, j],3} ");
+        };
+    }
+}
+
+void DecreasingRows(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int count = 0; count < matrix.GetLength(1) - 1; count++)
+        {
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] < matrix[i, j + 1])
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                }                
+            }
+        };
+    }
+}
+
+int[,] createMatrixRndInt = CreateMatrixRndInt(4, 5, 1, 100);
+PrintMatrix(createMatrixRndInt);
+Console.WriteLine(string.Empty);
+
+DecreasingRows(createMatrixRndInt);
+Console.WriteLine(string.Empty);
+PrintMatrix(createMatrixRndInt);
